6aa5deb6f7d1c0907610b39eed77eec09195b817
Rishiraj Singh Hada <rrajsinghhada@gmail.com>
2025-01-04 10:43:24 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TextComparison from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TextComparison />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
